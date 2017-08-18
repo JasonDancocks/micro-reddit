efe420e8df8a07a8eeecee125821c6cf62a96632
Jason Dancocks <jason.dancocks@gmail.com>
2017-08-18 18:47:50 +0100

    finish micro reddit project

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+	validates :username, presence:true, uniqueness: {case_sensitive: false}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255}, 
+																		format: { with: VALID_EMAIL_REGEX },
+																		uniqueness: {case_sensitive: false}
+	validates :password, presence: true, length: {minimum: 6 }
+
+	has_many :posts
+	has_many :comments
+end
---
+++ db/migrate/20170818172553_adduseridtoposts.rb
@@ -0,0 +1,4 @@
+class Adduseridtoposts < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
